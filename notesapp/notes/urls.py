a2b4b858e340ba1510e4c06b9c70ad30dc810aff
LAStickURFU <Anton.Levchenko@urfu.me>
2024-06-03 00:20:22 +0500

    first commit

---
+++ notesapp/notes/urls.py
@@ -0,0 +1,11 @@
+# notes/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', views.add_note, name='add_note'),
+    path('edit/<int:note_id>/', views.edit_note, name='edit_note'),
+    path('detail/<int:note_id>/', views.detail_note, name='detail_note'),
+    path('delete/<int:note_id>/', views.delete_note, name='delete_note'),
+]
---
+++ notesapp/notes/views.py
@@ -0,0 +1,40 @@
+# notes/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Note
+from .forms import NoteForm
+
+def index(request):
+    notes = Note.objects.all()
+    return render(request, 'notes/index.html', {'notes': notes})
+
+def add_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/add_note.html', {'form': form})
+
+def edit_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'notes/edit_note.html', {'form': form, 'note': note})
+
+def detail_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    return render(request, 'notes/detail_note.html', {'note': note})
+
+def delete_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    if request.method == 'POST':
+        note.delete()
+        return redirect('index')
+    return render(request, 'notes/delete_note.html', {'note': note})
